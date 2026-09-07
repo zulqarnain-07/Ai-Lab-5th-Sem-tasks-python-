3ee45f14eef805738a58e7077dd78ab8f6874161
Zulqarnain <thatshigh78@gmail.com>
2026-09-07 20:53:22 +0500

    task1

---
+++ Lab1/task1.py
@@ -0,0 +1,5 @@
+# Task 1: AI Welcome Program
+# Write a Python program that asks the user to enter their name and displays a welcome message.
+
+name = input("Enter your name: ")
+print("Welcome to AI,", name)
